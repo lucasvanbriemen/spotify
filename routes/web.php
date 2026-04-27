43457c23d5747d11035853f70f5f06b70b371551
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-04-27 22:04:47 +0200

    Keep old route.

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,11 @@
 use App\Http\Controllers\SpotifyAuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
-
 Route::get('/auth/spotify', [SpotifyAuthController::class, 'redirect']);
 Route::get('/auth/spotify/callback', [SpotifyAuthController::class, 'callback']);
 Route::get('/auth/spotify/logout', [SpotifyAuthController::class, 'logout']);
 Route::get('/api/spotify/token', [SpotifyAuthController::class, 'token']);
+
+Route::get('{any}', function () {
+    return view('index');
+})->where('any', '.*')->name('spa.catchall');
