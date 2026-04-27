98cb3532741a71c1ad41f85dbcdc37272c32dc81
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-04-27 22:03:15 +0200

    Expose routes.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,30 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Spotify</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <script src="https://sdk.scdn.co/spotify-player.js"></script>
     @vite(['resources/js/main.js'])
 </head>
 <body>
+    <script>
+        const API_ROUTES = [];
+        const currentDomain = window.location.origin;
+
+        function route(name, params = {}) {
+            const route = API_ROUTES.find(r => r.name === name);
+            let uri = route.uri;
+
+            for (const [key, value] of Object.entries(params)) {
+                const cleanKey = key.startsWith('$') ? key.slice(1) : key;
+                uri = uri.replace(`{${cleanKey}}`, encodeURIComponent(value));
+            }
+
+            return `/${uri}`;
+        }
+    </script>
+
     <div id="app"></div>
 </body>
 </html>
