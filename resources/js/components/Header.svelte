ca7ee0df6353a7d09511706ec5994c09755d68e4
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-04-29 22:05:13 +0200

    Use descriptive style for Git diff output.

--- resources/js/components/Header.svelte
+++ resources/js/components/Header.svelte
@@ -1,19 +1,15 @@
 <script>
-  import { onMount } from 'svelte';
   import Icon from './Icon.svelte';
+  import '../../scss/header.scss';
 
 </script>
 
 <header>
   <a class="logo" href="#/">
     <Icon name="logo" />
-    <span class="title">Git</span>
+    <span class="title">Music</span>
   </a>
 
   <div class="separator"></div>
 
 </header>
-
-<style>
-  @import "../../scss/header.scss";
-</style>
