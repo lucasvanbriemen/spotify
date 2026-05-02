99d9c8e0e06e4c396dc3c26a4b9baf25dca16657
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-05-02 16:14:14 +0200

    Fix line spacing in Git diff output.

--- routes/api.php
+++ routes/api.php
@@ -8,8 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(IsLoggedIn::class)->group(function () {
     Route::get('/search', [SpotifyController::class, 'search'])->name('search');
     Route::get('/get-mp3-url', [SpotifyController::class, 'getMp3Url'])->name('get-mp3-url');
-    Route::get('/audio/{id}', [SpotifyController::class, 'streamMp3'])
-        ->name('stream-mp3');
+    Route::get('/audio/{id}', [SpotifyController::class, 'streamMp3'])->name('stream-mp3');
 
     Route::get('/playlists', [PlaylistController::class, 'index'])->name('playlists');
 });
