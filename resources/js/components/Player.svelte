33d741fabab7e19eaed10eee8fe778f9ee924159
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-05-02 16:26:06 +0200

    Remove mute for now.

--- resources/js/components/Player.svelte
+++ resources/js/components/Player.svelte
@@ -5,19 +5,12 @@
   let audioEl;
   let isPlaying = $state(false);
   let duration = $state(0);
-  let isMuted = $state(false);
   let isSeeking = $state(false);
   let pendingSeek = $state(false);
   let seekValue = $state(0);
-  let volumeSlider = $state(0.7);
 
   let sliderMax = $derived(duration || 100);
   let progressPct = $derived((seekValue / sliderMax) * 100);
-  let volumePct = $derived((isMuted ? 0 : volumeSlider) * 100);
-
-  $effect(() => {
-    if (audioEl) audioEl.volume = isMuted ? 0 : volumeSlider;
-  });
 
   $effect(() => {
     if (audioEl && $currentlyPlaying.stream_url) {
@@ -40,10 +33,6 @@
     if (!audioEl) return;
     if (audioEl.paused) audioEl.play().catch(() => {});
     else audioEl.pause();
-  }
-
-  function toggleMute() {
-    isMuted = !isMuted;
   }
 
   function onSeekInput() {
@@ -110,26 +99,7 @@
     </div>
   </div>
 
-  <div class="extras">
-    <button class="ctrl" onclick={toggleMute} aria-label="Mute">
-      {#if isMuted || volumeSlider === 0}
-        <svg viewBox="0 0 24 24"><path d="M11 5L6 9H2v6h4l5 4V5zM23 9l-6 6M17 9l6 6"/></svg>
-      {:else if volumeSlider < 0.5}
-        <svg viewBox="0 0 24 24"><path d="M11 5L6 9H2v6h4l5 4V5zM15.5 8.5a5 5 0 010 7"/></svg>
-      {:else}
-        <svg viewBox="0 0 24 24"><path d="M11 5L6 9H2v6h4l5 4V5zM15.5 8.5a5 5 0 010 7M19 5a9 9 0 010 14"/></svg>
-      {/if}
-    </button>
-    <input
-      type="range"
-      class="slider slider--volume"
-      style="--fill:{volumePct}%"
-      min="0"
-      max="1"
-      step="0.01"
-      bind:value={volumeSlider}
-    />
-  </div>
+  <div></div>
 
   <audio
     bind:this={audioEl}
