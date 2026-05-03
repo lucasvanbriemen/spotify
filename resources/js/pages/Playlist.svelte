61eb70a79988463d018f8acfb61f4e3ec13e1cfe
Lucas van Briemen <contact@lucasvanbriemen.nl>
2026-05-03 15:14:36 +0200

    Improve style details.

--- resources/js/pages/Playlist.svelte
+++ resources/js/pages/Playlist.svelte
@@ -8,14 +8,18 @@
 
   onMount(async () => {
     playlist = await api.get(route('playlist.show', { playlist: id }));
+    playlist.duration = playlist.songs.reduce((total, song) => total + song.duration_ms, 0);
   });
 </script>
 
-<div class="header">
-  <img src={playlist.image_url} alt={playlist.name} />
-  <div class="overlay"></div>
-  
-  <div class="info">
-    <span class="title">{playlist.name}</span>
+{#if playlist.songs}
+  <div class="header">
+    <img src={playlist.image_url} alt={playlist.name} />
+    <div class="overlay"></div>
+
+    <div class="info">
+      <span class="title">{playlist.name}</span>
+      <span class="details">{playlist.songs.length} songs, {Math.floor(playlist.duration / 60000)} min</span>
+    </div>
   </div>
-</div>
+{/if}
