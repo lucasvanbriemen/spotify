871a3a9709f7ecc2d34c1c23ce5865e112e47a6e
Lucas <contact@lucasvanbriemen.nl>
2026-05-10 12:05:04 +0200

    Top align.

--- ios/music/music/ContentView.swift
+++ ios/music/music/ContentView.swift
@@ -4,35 +4,43 @@ struct ContentView: View {
     @State var playlists: [Playlist] = []
     
     var body: some View {
+        
+            
+  
         NavigationStack {
-            ForEach(playlists) { group in
-                NavigationLink(destination: PlaylistView()) {
-                    ZStack {
-                        if group.image == nil {
-                            EmptyView()
-                        } else {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    
+                    ForEach(playlists) { group in
+                        NavigationLink(destination: PlaylistView()) {
                             ZStack {
-                                
-                                
-                                AsyncImage(url: URL(string: group.image!)) { image in
-                                    image.resizable()
-                                        .blur(radius: 1)
-                                } placeholder: {
-                                    ProgressView()
+                                if group.image == nil {
+                                    EmptyView()
+                                } else {
+                                    ZStack {
+                                        
+                                        
+                                        AsyncImage(url: URL(string: group.image!)) { image in
+                                            image.resizable()
+                                                .blur(radius: 1)
+                                        } placeholder: {
+                                            ProgressView()
+                                        }
+                                        .frame(width: 400, height: 250)
+                                        .clipShape(RoundedRectangle(cornerRadius: 32))
+                                        
+                                        
+                                        LinearGradient(colors: [.clear, .black.opacity(0.8)], startPoint: .top, endPoint: .bottom)
+                                            .frame(width: 400, height: 250)
+                                            .clipShape(RoundedRectangle(cornerRadius: 32))
+                                        
+                                    }
                                 }
-                                .frame(width: 400, height: 250)
-                                .clipShape(RoundedRectangle(cornerRadius: 32))
-                                
-                                
-                                LinearGradient(colors: [.clear, .black.opacity(0.8)], startPoint: .top, endPoint: .bottom)
-                                    .frame(width: 400, height: 250)
-                                    .clipShape(RoundedRectangle(cornerRadius: 32))
-                                
+                                Text(group.name)
+                                    .foregroundStyle(Color.white)
+                                    .font(Font.largeTitle.bold())
                             }
                         }
-                        Text(group.name)
-                            .foregroundStyle(Color.white)
-                            .font(Font.largeTitle.bold())
                     }
                 }
             }
