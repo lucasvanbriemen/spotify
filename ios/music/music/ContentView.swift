c6c0ca76946cca1dafb027a6494c49aaaee4320c
Lucas <contact@lucasvanbriemen.nl>
2026-05-10 08:54:02 +0200

    Initialize iOS project with basic structure.

---
+++ ios/music/music/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  music
+//
+//  Created by Lucas on 10/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios/music/music/musicApp.swift
@@ -0,0 +1,17 @@
+//
+//  musicApp.swift
+//  music
+//
+//  Created by Lucas on 10/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct musicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
