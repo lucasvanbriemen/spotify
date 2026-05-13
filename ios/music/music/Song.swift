23fc611942c8f5ebca8e85ea3bfd20da3d6b5120
Lucas <contact@lucasvanbriemen.nl>
2026-05-13 17:16:26 +0200

    Change object properties and keys in Song class.

--- ios/music/music/Song.swift
+++ ios/music/music/Song.swift
@@ -2,16 +2,15 @@ import Foundation
 
 class Song: Codable, Identifiable {
     var id: Int
-    var name: String
-    var durationMS: Int
-    var mp3Url: String?
+    var title: String
+    var duration: Int
+    var fileId: String?
     var imageUrl: String?
     var artist: String?
     
     enum CodingKeys: String, CodingKey {
-        case id, name, artist
-        case durationMS = "duration_ms"
-        case mp3Url = "mp3_url"
+        case id, title, artist, duration
+        case fileId = "file_id"
         case imageUrl = "image_url"
     }
 }
