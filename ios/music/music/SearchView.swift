4d60181c2d8db929d1f59ad851949df6459837a5
Lucas <contact@lucasvanbriemen.nl>
2026-05-13 14:39:58 +0200

    Search songs.

--- ios/music/music/SearchView.swift
+++ ios/music/music/SearchView.swift
@@ -2,13 +2,22 @@ import SwiftUI
 
 struct SearchView: View {
     @State var searchText: String = ""
+    @State var songs: [Song] = []
     
     var body: some View {
         NavigationStack {
             VStack {
-                Text("Search View")
+                Text("Search")
+                
+                ForEach(songs) { song in
+                    SongListingView(song: song, bgColor: Color.clear, playlistID: 1)
+                }
             }
         }
         .searchable(text: $searchText)
+    }
+    
+    func getSongs() async {
+        songs = await SeverApi.get(endpoint: "songs?q=\(searchText)") ?? []
     }
 }
