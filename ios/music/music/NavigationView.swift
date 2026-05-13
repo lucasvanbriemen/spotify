68153c35c09f8ea38a6afc3dd18decc6b7c19c63
Lucas <contact@lucasvanbriemen.nl>
2026-05-13 14:06:02 +0200

    Separate search tab.

--- ios/music/music/NavigationView.swift
+++ ios/music/music/NavigationView.swift
@@ -1,10 +1,10 @@
 import SwiftUI
 
-struct NavigationView: View {
+struct NavigationView: View {    
     var body: some View {
         TabView {
             Tab("Playlists", systemImage: "play.square.stack", content: { PlaylistOverviewView() })
-            Tab("Search", systemImage: "magnifyingglass", content: { PlaylistOverviewView() })
+            Tab(role: .search, content: { SearchView() })
         }
         .tabViewBottomAccessory(isEnabled: PlayerManager.shared.currentlyPlaying != nil, content: { PlayerView() })
         .tabBarMinimizeBehavior(TabBarMinimizeBehavior.onScrollDown)
