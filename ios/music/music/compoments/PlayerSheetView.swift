d9cf56c17f403f6f2917ae767150131729d697fc
Lucas <contact@lucasvanbriemen.nl>
2026-05-14 14:31:03 +0200

    Clean up view.

--- ios/music/music/compoments/PlayerSheetView.swift
+++ ios/music/music/compoments/PlayerSheetView.swift
@@ -3,28 +3,19 @@ import AVFoundation
 
 struct PlayerSheetView: View {
     @State private var manager = PlayerManager.shared
-
-    @State private var speed = 50.0
-    @State private var isEditing = false
-
     
     var body: some View {
         SongListingView(song: manager.currentlyPlaying!, bgColor: Color.clear, shouldPlaySong: false)
 
-        Slider(
-                value: $manager.timeIntoSong,
-                in: 0...Double(manager.currentlyPlaying!.duration),
-            ) {
-                Text("Speed")
-            } minimumValueLabel: {
-                Text("0")
-            } maximumValueLabel: {
-                Text(String(manager.currentlyPlaying!.duration))
-            } onEditingChanged: { editing in
-                if editing { return }
-                manager.player?.seek(to: CMTime(value: Int64(manager.timeIntoSong), timescale: 1))
-            }
-            Text("\(manager.timeIntoSong)")
-                .foregroundColor(isEditing ? .red : .blue)
+        Slider(value: $manager.timeIntoSong, in: 0...Double(manager.currentlyPlaying!.duration)) {
+            Text("Speed")
+        } minimumValueLabel: {
+            Text("0")
+        } maximumValueLabel: {
+            Text(String(manager.currentlyPlaying!.duration))
+        } onEditingChanged: { editing in
+            if editing { return }
+            manager.player?.seek(to: CMTime(value: Int64(manager.timeIntoSong), timescale: 1))
+        }
     }
 }
