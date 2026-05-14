6eaadeed7318c445cb0596d67febc90ef67a17eb
Lucas <contact@lucasvanbriemen.nl>
2026-05-14 15:12:15 +0200

    Add controls to sheet.

--- ios/music/music/compoments/PlayerSheetView.swift
+++ ios/music/music/compoments/PlayerSheetView.swift
@@ -7,6 +7,42 @@ struct PlayerSheetView: View {
     var body: some View {
         SongListingView(song: manager.currentlyPlaying!, bgColor: Color.clear, shouldPlaySong: false)
 
+        
+        Button(action: { manager.playPreviousSong() }) {
+            Image(systemName: "arrowtriangle.down.2.fill")
+                .font(Font.system(size: 32))
+        }
+        .frame(width: 56, height: 56)
+        .clipShape(Circle())
+        .background(Color.accentColor)
+        .foregroundStyle(Color.white)
+        .clipShape(Circle())
+        .rotationEffect(Angle(degrees: 90))
+        
+        
+        Button(action: { manager.togglePlayPause() }) {
+            Image(systemName: manager.isPlaying ? "pause" : "play")
+                .font(Font.system(size: 32))
+        }
+        .frame(width: 56, height: 56)
+        .clipShape(Circle())
+        .background(Color.accentColor)
+        .foregroundStyle(Color.white)
+        .clipShape(Circle())
+        
+        Button(action: { manager.playNextSong() }) {
+            Image(systemName: "arrowtriangle.up.2.fill")
+                .font(Font.system(size: 32))
+        }
+        .frame(width: 56, height: 56)
+        .clipShape(Circle())
+        .background(Color.accentColor)
+        .foregroundStyle(Color.white)
+        .clipShape(Circle())
+        .rotationEffect(Angle(degrees: 90))
+        
+        
+        
         Slider(value: $manager.timeIntoSong, in: 0...Double(manager.currentlyPlaying!.duration)) {
             Text("Seek")
         } minimumValueLabel: {
@@ -20,3 +56,4 @@ struct PlayerSheetView: View {
         }
     }
 }
+
