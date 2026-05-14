333f8eae5b3fe3447e6f4493978212ce3da1c97f
Lucas <contact@lucasvanbriemen.nl>
2026-05-14 15:41:11 +0200

    Refactor play controls layout in PlayerSheetView.

--- ios/music/music/compoments/PlayerSheetView.swift
+++ ios/music/music/compoments/PlayerSheetView.swift
@@ -8,38 +8,40 @@ struct PlayerSheetView: View {
         SongListingView(song: manager.currentlyPlaying!, bgColor: Color.clear, shouldPlaySong: false)
 
         
-        Button(action: { manager.playPreviousSong() }) {
-            Image(systemName: "arrowtriangle.down.2.fill")
-                .font(Font.system(size: 32))
+        HStack {
+            Button(action: { manager.playPreviousSong() }) {
+                Image(systemName: "arrowtriangle.down.2.fill")
+                    .font(Font.system(size: 32))
+            }
+            .frame(width: 56, height: 56)
+            .clipShape(Circle())
+            .background(Color.accentColor)
+            .foregroundStyle(Color.white)
+            .clipShape(Circle())
+            .rotationEffect(Angle(degrees: 90))
+            
+            
+            Button(action: { manager.togglePlayPause() }) {
+                Image(systemName: manager.isPlaying ? "pause" : "play")
+                    .font(Font.system(size: 32))
+            }
+            .frame(width: 56, height: 56)
+            .clipShape(Circle())
+            .background(Color.accentColor)
+            .foregroundStyle(Color.white)
+            .clipShape(Circle())
+            
+            Button(action: { manager.playNextSong() }) {
+                Image(systemName: "arrowtriangle.up.2.fill")
+                    .font(Font.system(size: 32))
+            }
+            .frame(width: 56, height: 56)
+            .clipShape(Circle())
+            .background(Color.accentColor)
+            .foregroundStyle(Color.white)
+            .clipShape(Circle())
+            .rotationEffect(Angle(degrees: 90))
         }
-        .frame(width: 56, height: 56)
-        .clipShape(Circle())
-        .background(Color.accentColor)
-        .foregroundStyle(Color.white)
-        .clipShape(Circle())
-        .rotationEffect(Angle(degrees: 90))
-        
-        
-        Button(action: { manager.togglePlayPause() }) {
-            Image(systemName: manager.isPlaying ? "pause" : "play")
-                .font(Font.system(size: 32))
-        }
-        .frame(width: 56, height: 56)
-        .clipShape(Circle())
-        .background(Color.accentColor)
-        .foregroundStyle(Color.white)
-        .clipShape(Circle())
-        
-        Button(action: { manager.playNextSong() }) {
-            Image(systemName: "arrowtriangle.up.2.fill")
-                .font(Font.system(size: 32))
-        }
-        .frame(width: 56, height: 56)
-        .clipShape(Circle())
-        .background(Color.accentColor)
-        .foregroundStyle(Color.white)
-        .clipShape(Circle())
-        .rotationEffect(Angle(degrees: 90))
         
         
         
