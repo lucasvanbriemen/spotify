a75222567adb29f70e2a45e2f097614c2f75919b
Lucas <contact@lucasvanbriemen.nl>
2026-05-15 17:45:43 +0200

    Cast to strings.

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -11,6 +11,10 @@ class Playlist extends Model
         'image_url',
     ];
 
+    protected $casts = [
+        'id' => 'string',
+    ];
+
     public function songs()
     {
         return $this->belongsToMany(Song::class, 'playlist_songs', 'playlist_id', 'song_isrc', 'id', 'isrc')->withTimestamps();
