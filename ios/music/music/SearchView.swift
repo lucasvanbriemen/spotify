201f492c30a2e5de6be8cf647697c70fb0fbc05b
Lucas <contact@lucasvanbriemen.nl>
2026-05-15 19:07:21 +0200

    Remove empty line.

--- ios/music/music/SearchView.swift
+++ ios/music/music/SearchView.swift
@@ -29,7 +29,7 @@ struct SearchView: View {
                             }
                         }
                     }
-∂                }
+                }
                 .padding([.trailing, .leading], 8)
             }
         }
